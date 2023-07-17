bbcfbae92b11ae16ffbaaa46c4b9599ec35b7a83
dantegarciadev <zickdante2015@gmail.com>
2023-07-17 02:01:41 -0300

    botones para minimizar y maximizar

--- sm_app.py
+++ sm_app.py
@@ -7,6 +7,29 @@ from flet import *
 
 def main(page: Page):
     
+    #Funcion minimizar
+    def _min(e):
+        _b.controls[0].width =110
+        _b.controls[0].scale = transform.Scale(0.9, alignment.center_right)
+        _b.controls[0].border_radius = border_radius.only(
+            top_right=0, bottom_right=0,top_left=35,bottom_left=35,
+        )
+        
+        #desabilitar
+        _b.controls[0].disabled =True
+        _b.controls[0].update()
+        pass
+    
+    
+    _a=Container(
+        width=300,
+        height=550,
+        bgcolor="#900b04",
+        border_radius=35,
+    )
+    
+    _card_container=Row(scroll="auto")
+    
     #controles del dashboard
     _top = Container(
         gradient=LinearGradient(
@@ -24,14 +47,15 @@ def main(page: Page):
                         controls=[
                             Text(
                                 "Signo Medico",
-                                col={"xs": 6},
+                                col={"xs": 8},
                                 no_wrap=True, #se setea a verdadero asi se puede crear el efecto slider despues
                                 size=20,
                                 weight="bold", 
                                 color= "#ffffff",
                             ),
                             Container(
-                                col={"xs":1},
+                                col={"xs":2},
+                                on_click=lambda e: _min(e),
                                 content=Text(
                                     'ⓘ',
                                     weight='bold', no_wrap=True,
@@ -46,8 +70,11 @@ def main(page: Page):
                     'CATEGORIES',
                     size=12,
                     color="white60",
-                    no_wrap=True,
-                    
+                    no_wrap=True,    
+                ),
+                Container(
+                    padding=padding.only(top=10,bottom=20),
+                    content=_card_container,   
                 )
             ]
             
@@ -79,6 +106,64 @@ def main(page: Page):
         ],
     )
     
+    #_card_container
+    
+    l=["convenio1","convenio2","convenio2"]
+    t=["transformar1","transformar2","transformar3"]
+    p=[90,123,45]
+   
+    
+    for index in range(3):
+        _=Card(elevation=15,
+               content=Container(
+                   width=160,
+                   height=100,
+                   padding=15,
+                   content=Column(
+                        #Muestra seteable UI => para ser modificada despues
+                        alignment="spaceBetween",
+                        controls=[
+                            Container(
+                                content=Column(
+                                    spacing=3,
+                                    controls=[
+                                        Text(
+                                            #tomo los elementos del index
+                                            t[index],
+                                            color="white60",
+                                            size=12,
+                                        ),
+                                        Text(
+                                            #aca igual
+                                            l[index],
+                                            size=20,
+                                        ),
+                                    ],
+                                    
+                                )
+                            ),
+                            #se puede usar como la barra de progreso
+                            Container(
+                                width=160,
+                                height=5,
+                                bgcolor="white12",
+                                border_radius=20,
+                                padding=padding.only(right=p[index]),
+                                content=Container(
+                                    bgcolor="pink",
+                                ),
+                            ),
+                        ],
+                   )
+               ),
+            )
+        
+        
+        _card_container.controls.append(_)
+    
+    
+    
+    
     #contenedor prinpal
     _c = Container(
         width = 300,
@@ -90,6 +175,7 @@ def main(page: Page):
             width=300,
             height=550,
             controls=[
+                _a,
                 _b,
             ]
             
