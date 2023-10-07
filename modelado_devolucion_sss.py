673be929c0d837c25433e1c8fb203339e9407dba
dantegarciadev <zickdante205@gmail.com>
2023-10-06 22:30:33 -0300

    agregdos botones

--- modelado_devolucion_sss.py
+++ modelado_devolucion_sss.py
@@ -5,21 +5,65 @@ from flet import *
 
 
 def main(page: Page):
-    page.appbar=ft.AppBar(
-        title=ft.Text("SIGNO MEDICO", italic=True),center_title=True,
+  page.appbar = ft.AppBar(
+    title=ft.Text("SIGNO MEDICO", italic=True),
+    center_title=True, 
+  )
+  page.window_max_width=1200
+  page.window_max_height=700
+  page.title = "SIGNO MEDICO"
+  page.bgcolor = "#c4c4c4"
+  container=ft.Container(
+        top=ft.Row(
+            [
+                ft.ElevatedButton("procesar SSS",width=300,height=50),
+                ft.ElevatedButton("procesar txt",width=300,height=50),
+                ft.ElevatedButton("exportar",width=300,height=50),
+            ],
+            wrap=True,
+            spacing=10,
+            run_spacing=10,
+            width=page.window_width,
+            alignment=ft.MainAxisAlignment.CENTER,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+        ),
+        _o=ft.Row(
+            [
+                ft.ElevatedButton("procesar SSS",width=300,height=50),
+                ft.ElevatedButton("procesar txt",width=300,height=50),
+                ft.ElevatedButton("exportar",width=300,height=50),
+            ],
+            wrap=True,
+            spacing=10,
+            run_spacing=10,
+            width=page.window_width,
+            alignment=ft.MainAxisAlignment.CENTER,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+        ),
+        buton=ft.Row(
+            [
+                ft.ElevatedButton("procesar SSS",width=300,height=50),
+                ft.ElevatedButton("procesar txt",width=300,height=50),
+                ft.ElevatedButton("exportar",width=300,height=50),
+            ],
+            wrap=True,
+            spacing=10,
+            run_spacing=10,
+            width=page.window_width,
+            alignment=ft.MainAxisAlignment.CENTER,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+        ),
+        
+    bgcolor=ft.colors.YELLOW,
+    alignment=ft.alignment.center,
 
-    )
-    row = ft.Row(
-    wrap=True,
-    spacing=10,
-    run_spacing=10,
-    width=page.window_width,
-    bgcolor=ft.colors.YELLOW
-    )
-    page.title="SIGNO MEDICO"
-    page.bgcolor = "#c4c4c4"
-    page.add(ft.Text("HOLAA FLET"),
-             row
-        )
+  )
+
+
+
+
+  page.add(container
+
+  )
 
 ft.app(target=main)
